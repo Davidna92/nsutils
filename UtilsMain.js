80f0cf679bdc01e8ee7b3330998ed2ad164dc839
DavidNahum92 <david.n@top-group.co.il>
2025-01-14 11:31:43 +0200

    Create UtilsMain.js
    hello function - example

---
+++ UtilsMain.js
@@ -0,0 +1,18 @@
+/**
+ * @NApiVersion 2.1
+ */
+define([],
+
+    () => {
+
+        const helloFunction = (name) => {
+            return `Hello ${name}`;
+        }
+
+        const bar = () => {
+
+        }
+
+        return {helloFunction, bar}
+
+    });
